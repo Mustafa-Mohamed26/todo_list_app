1c0e8f2bf1de244f02dbc776bdab1143a10d0196
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-03-01 21:12:11 +0200

    modify the note details page

--- lib/screens/note_detail_screen.dart
+++ lib/screens/note_detail_screen.dart
@@ -4,6 +4,8 @@ import 'package:todo_list_app/models/todo_model.dart';
 import 'package:todo_list_app/providers/todo_provider.dart';
 import 'package:todo_list_app/widgets/customDiscripInput.dart';
 import 'package:todo_list_app/widgets/customTitleInput.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:todo_list_app/widgets/priority_radio_button.dart';
 
 class NoteDetailScreen extends StatefulWidget {
   final Todo todo;
@@ -17,10 +19,24 @@ class NoteDetailScreen extends StatefulWidget {
 class _NoteDetailScreenState extends State<NoteDetailScreen> {
   late TextEditingController titleController;
   late TextEditingController descriptionController;
+  late TextEditingController deadlineController;
+  late TextEditingController timeController;
 
   late TodoProvider todoProvider;
 
   final GlobalKey<FormState> formState = GlobalKey<FormState>();
+
+  late Todo originalTodo;
+
+  int? selectedPriority;
+
+  List<String> categories = [
+    'Work',
+    'Education',
+    'Shopping',
+    'Personal',
+    'Home',
+  ];
 
   @override
   void initState() {
@@ -29,13 +45,47 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
     descriptionController = TextEditingController(
       text: widget.todo.description,
     );
+    deadlineController = TextEditingController(
+      text:
+          widget.todo.deadline != null
+              ? "${widget.todo.deadline!.year}-${widget.todo.deadline!.month.toString().padLeft(2, '0')}-${widget.todo.deadline!.day.toString().padLeft(2, '0')}"
+              : '',
+    );
+    timeController = TextEditingController(
+      text:
+          widget.todo.time != null
+              ? "${widget.todo.time!.hour.toString().padLeft(2, '0')}:${widget.todo.time!.minute.toString().padLeft(2, '0')}"
+              : '',
+    );
+    originalTodo = Todo(
+      id: widget.todo.id,
+      title: widget.todo.title,
+      description: widget.todo.description,
+      isCompleted: widget.todo.isCompleted,
+      category: widget.todo.category,
+      deadline: widget.todo.deadline,
+      priority: widget.todo.priority,
+      time: widget.todo.time,
+    );
+    selectedPriority = widget.todo.priority;
   }
 
   @override
   void dispose() {
     titleController.dispose();
     descriptionController.dispose();
+    deadlineController.dispose();
+    timeController.dispose();
     super.dispose();
+  }
+
+  bool hasChanges() {
+    return originalTodo.title != titleController.text ||
+        originalTodo.description != descriptionController.text ||
+        originalTodo.category != widget.todo.category ||
+        originalTodo.deadline != widget.todo.deadline ||
+        originalTodo.time != widget.todo.time ||
+        originalTodo.priority != selectedPriority;
   }
 
   void updateTodo() {
@@ -45,12 +95,22 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
         title: titleController.text,
         description: descriptionController.text,
         isCompleted: widget.todo.isCompleted,
-        category: '',
-        deadline: null,
-        priority: 1, time: null,
+        category: widget.todo.category,
+        deadline: widget.todo.deadline,
+        priority: selectedPriority ?? 1,
+        time: widget.todo.time,
       );
       todoProvider.updateTodo(updatedTodo);
-      Navigator.pop(context);
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.success,
+        animType: AnimType.bottomSlide,
+        title: 'Updated',
+        desc: 'The todo has been updated successfully!',
+        btnOkOnPress: () {
+          Navigator.pop(context); // Close the screen after confirmation
+        },
+      ).show();
     }
   }
 
@@ -58,77 +118,279 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
     if (widget.todo.id != null) {
       todoProvider.deleteTodo(widget.todo.id!);
     }
-    Navigator.pop(context);
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.success,
+      animType: AnimType.bottomSlide,
+      title: 'Deleted',
+      desc: 'The todo has been deleted successfully!',
+      btnOkOnPress: () {
+        Navigator.pop(context); // Close the screen after confirmation
+      },
+    ).show();
   }
 
   @override
   Widget build(BuildContext context) {
     todoProvider = Provider.of<TodoProvider>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Note Details",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        actions: [
-          Container(
-            margin: EdgeInsets.only(right: 16),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
+    return WillPopScope(
+      onWillPop: () async {
+        if (hasChanges()) {
+          AwesomeDialog(
+            context: context,
+            dialogType: DialogType.info,
+            animType: AnimType.bottomSlide,
+            title: 'Changes Made',
+            desc: 'You have made changes to the todo item.',
+            btnOkOnPress: () {
+              updateTodo();
+            },
+            btnCancelOnPress: () {
+              Navigator.pop(context);
+            },
+          ).show();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Note Details",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            Container(
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.red, width: 1),
+              ),
+              child: IconButton(
+                icon: Icon(Icons.delete, size: 30),
                 color: Colors.red,
-                width: 1,
+                onPressed: deleteTodo,
               ),
             ),
-            child: IconButton(
-              icon: Icon(Icons.delete, size: 30),
-              color: Colors.red,
-              onPressed: deleteTodo,
-            ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: formState,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CustomTitleInput(
-                  myController: titleController,
-                  valid: (val) {
-                    if (val!.isEmpty) {
-                      return "Field is empty";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 10),
-                CustomDiscripinput(myController: descriptionController),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: updateTodo,
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 10,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: formState,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomTitleInput(
+                    myController: titleController,
+                    valid: (val) {
+                      if (val!.isEmpty) {
+                        return "Field is empty";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  CustomDiscripinput(myController: descriptionController),
+                  SizedBox(height: 20),
+                  DropdownButtonFormField<String>(
+                    value: widget.todo.category,
+                    decoration: InputDecoration(
+                      labelText: 'Category',
+                      labelStyle: TextStyle(color: Colors.blue),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    items:
+                        categories.map((String category) {
+                          return DropdownMenuItem<String>(
+                            value: category,
+                            child: Text(category),
+                          );
+                        }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        widget.todo.category = newValue!;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a category';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: deadlineController,
+                    decoration: InputDecoration(
+                      labelText: 'Deadline',
+                      labelStyle: TextStyle(color: Colors.blue),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.calendar_today,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: widget.todo.deadline ?? DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2101),
+                        builder: (context, child) {
+                          return Theme(
+                            data: Theme.of(context).copyWith(
+                              colorScheme: ColorScheme.light(
+                                primary: Colors.blue,
+                                onPrimary: Colors.white,
+                                onSurface: Colors.blue,
+                              ),
+                              textButtonTheme: TextButtonThemeData(
+                                style: TextButton.styleFrom(),
+                              ),
+                            ),
+                            child: child!,
+                          );
+                        },
+                      );
+                      if (pickedDate != null) {
+                        setState(() {
+                          widget.todo.deadline = pickedDate;
+                          deadlineController.text =
+                              "${pickedDate.year}-${pickedDate.month.toString().padLeft(2, '0')}-${pickedDate.day.toString().padLeft(2, '0')}";
+                        });
+                      }
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a deadline';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: timeController,
+                    decoration: InputDecoration(
+                      labelText: 'Time',
+                      labelStyle: TextStyle(color: Colors.blue),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Icon(Icons.access_time, color: Colors.blue),
+                    ),
+                    readOnly: true,
+                    onTap: () async {
+                      TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: widget.todo.time ?? TimeOfDay.now(),
+                        builder: (context, child) {
+                          return Theme(
+                            data: Theme.of(context).copyWith(
+                              colorScheme: ColorScheme.light(
+                                primary: Colors.blue,
+                                onPrimary: Colors.white,
+                                onSurface: Colors.blue,
+                              ),
+                              textButtonTheme: TextButtonThemeData(
+                                style: TextButton.styleFrom(),
+                              ),
+                            ),
+                            child: child!,
+                          );
+                        },
+                      );
+                      if (pickedTime != null) {
+                        setState(() {
+                          widget.todo.time = pickedTime;
+                          timeController.text =
+                              "${pickedTime.hour.toString().padLeft(2, '0')}:${pickedTime.minute.toString().padLeft(2, '0')}";
+                        });
+                      }
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a time';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Set Priority',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: PriorityRadioButton(
+                          label: 'Low',
+                          value: 1,
+                          selectedPriority: selectedPriority,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedPriority = value;
+                            });
+                          },
                         ),
                       ),
-                      child: Text('Update'),
-                    ),
-                  ],
-                ),
-              ],
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: PriorityRadioButton(
+                          label: 'Medium',
+                          value: 2,
+                          selectedPriority: selectedPriority,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedPriority = value;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: PriorityRadioButton(
+                          label: 'High',
+                          value: 3,
+                          selectedPriority: selectedPriority,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedPriority = value;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
